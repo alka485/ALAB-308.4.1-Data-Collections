795ff0afef9e9dc2e9dadf45ec22cf58fa09b7f1
Alka Sah <alkasah485@gmail.com>
2024-05-11 06:38:13 -0700

    modification in Part1

--- script.mjs
+++ script.mjs
@@ -15,7 +15,7 @@
 // Variables to hold string and cell data
 // const str = `ID,Name,Occupation,Age\n42,Bruce,Knight,41\n57,Bob,Fry Cook,19\n63,Blaine,Quiz Master,58\n98,Bill,Doctor's Assistant,26`;
 
-const str = `ID,Name,Occupation,Age\n42,Bruce,Knight,41\n57,Bob,Fry Cook,19\n63,Blaine,Quiz Master,58\n98,Bill,Doctor’s Assistant,26`
+const str = `ID,Name,Occupation,Age\n42,Bruce,Knight,41\n57,Bob,Fry Cook,19\n63,Blaine,Quiz Master,58\n98,Bill,Doctor’s Assistant,26,`
 
 let cell = "";
 let row =  [];
@@ -37,6 +37,10 @@ for (let i =0 ; i < str.length; i++){
  
     } else {
         cell += char;
+    }
+
+    if (row.length > 0) {
+        console.log(row.join(', '));
     }
 
 }
